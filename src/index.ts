6560dd73652c23a2a906fafd99eb569b6ec10eca
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-08-14 15:53:29 +0530

    add apis

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,24 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.post('/api/v1/user/signup', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/user/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!2')
+})
+
+app.get('/api/v1/blog/blog', (c) => {
+  return c.text('Hello Hono!2')
 })
 
 export default app
