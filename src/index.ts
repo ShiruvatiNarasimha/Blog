7ae32b3225858e7246a2d97275142888941692c2
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-08-15 09:58:09 +0530

    blog

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { Hono } from 'hono'
-
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { decode, sign, verify } from 'hono/jwt'
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
 
 
 // Very Impoartent
@@ -11,76 +9,10 @@ const app = new Hono<{
     DATABASE_URL: string;
     JWT_SECRET : string;
   }
-}>()
+}>();
 
-app.post('/api/v1/user/signup', async (c) => {
-   const body = await c.req.json()
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-}).$extends(withAccelerate())
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
 
-try {
-  const user = await prisma.user.create({
-    data : {
-      username : body.username,
-      password : body.password,
-      name : body.name
-    }
-  })
-  const jwt = await sign({
-    id: user.id
-  }, c.env.JWT_SECRET);
-    return c.text(jwt)
-  } catch(e) {
-    c.status(411);
-    return c.text("User already exits with this email")
-  }
-})
-
-
-
-app.post('/api/v1/user/signin', async (c) => {
-  const body = await c.req.json()
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-}).$extends(withAccelerate())
-
-try {
-  const user = await prisma.user.findFirst({
-    where : {
-      username : body.username,
-      password : body.password,
-    }
-  })
-  if (!user) {
-    c.status(403);
-    return c.json({
-      message : "Incorrect creds"
-    })
-
-  }
-  const jwt = await sign({
-    id: user.id
-  }, c.env.JWT_SECRET);
-  
-    return c.text(jwt)
-  } catch(e) {
-    c.status(411);
-    return c.text("Invalid")
-  }
-
-})
-
-app.post('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.get('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!2')
-})
-
-app.get('/api/v1/blog/blog', (c) => {
-  return c.text('Hello Hono!2')
-})
 
 export default app
