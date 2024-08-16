2ca74ef9f62d60314d8a69141335711925626850
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-08-17 04:01:19 +0530

    add routes

--- frontend/src/componets/Quote.tsx
+++ frontend/src/componets/Quote.tsx
@@ -1,5 +1,19 @@
 export const Quote = () => {
-    return <div className="bg-slate-200">
-
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg ">
+                <div className="text-3xl font-bold">
+                "The customer service I received was 
+                exceptional. The support team went above
+                and beyond to address my concers. "
+                </div> 
+                <div className="max-w-md  text-xl font-semibold text-left mt-4 ">
+                    Julies winfield
+        </div>
+        <div className="max-w-md  text-sm font-light text-slate-400">
+            CEO | Acne corp
+        </div>
+            </div>
+        </div>
     </div>
 }
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,7 +1,16 @@
+import { Auth } from "../componets/Auth"
+import { Quote } from "../componets/Quote"
 
 
 export const Signup = () => {
     return <div>
-        Signup
+        <div className="grid grid-cols-2">
+            <div>
+              <Auth/>
+            </div>
+            <div className="invisible lg:visible">
+                <Quote/>
+            </div>
+        </div>
     </div>
 }
