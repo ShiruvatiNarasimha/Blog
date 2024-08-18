8b62df766912b0a30105cef964567a3a9a6fc6e9
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-08-18 22:26:35 +0530

    Add Bolgs Routes

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,117 @@
 
 export const BACKEND_URL = "https://blog.mrnattyboy05.workers.dev"
+
+/*
+
+
+
+
+
+export const Auth = ({type}: {type: "signup" | "signin"}) => {
+   
+     return (  <div className ="h-screen flex justify-center flex-col">
+           <div className="flex- justify-center">
+              <div>
+                <div px-10>
+                  <div className="text-3xl font-extrabold">
+                   create an account
+                  </div> 
+                  <div className="text-slate-600">
+                       {type === "signin" ? "Don't have an account?" : "Already have an account?""}
+                    <Link className="pl-2 underline" to={type === "signin" ? "/signup": "/signin"}
+                    {type == "signin" ? "signup" : "signin"}
+                    </Link>
+                  </div>
+                </div>
+                <div className="pt-8">
+                {type === "signup" ? <LabelledInputs label="Username" type={"text" placeholder="Your Name.." onChange={(e)=> {
+                }}} }
+                
+                </div>
+              </div>
+
+           </div>
+
+       
+
+     </div>
+     
+     
+     )
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+interface LabelledInputsType {
+label : string;
+placholder: string;
+onChange: (e: ChangeEvent<HTMInput>) => void;
+
+}
+
+
+
+function LabelledInput({label, placholder, onChange, type}) {
+
+        return <div>
+        <label className= "block mb-2 text-sm font-bold text-black">{label}</label>
+        <input type={type || "text"} id="first_name" className="border-gray-300 text-gray-900 text-sm rounded-lg
+        focus:ring-blue-500 focus:boeder-blue-400 block w-full p-2.5"
+        placholder={placeholder} required />
+        </div>
+
+}
+
+
+
+
+
+
+
+
+
+
+*/
