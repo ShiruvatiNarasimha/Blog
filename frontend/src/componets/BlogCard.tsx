6ac6a981716aae9aa4b1209472283c8c71009c04
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-08-19 23:07:54 +0530

    add title

--- frontend/src/componets/BlogCard.tsx
+++ frontend/src/componets/BlogCard.tsx
@@ -6,35 +6,32 @@ interface BlogCardProps {
 }
 
 export const BlogCard = ({authorName, title, content, publishedDate}: BlogCardProps) => {
-    return <div>
+    return <div className="border border-slate-200 pb-4">
         <div className="flex">
             <div className="flex justify-center felx-col">
             <Avatar name={authorName}/>
             </div>
-           <div className="font-extralight">{authorName} </div>
-           <div className="pl-2 font-thin font-slate-200">
+           <div className="font-extralight pl-2">{authorName} . </div>
+           <div className="pl-2 font-thin text-slate-500">
            {publishedDate}
            </div>
         </div>
-        <div>
+        <div className="text-xl font-semibold">
             {title}
         </div>
-        <div>
-            {content.slice(0, 100) +"..."}
+        <div className="text-md font-thin">
+            {content.slice(0, 200) +"..."}
         </div>
-        <div>
-            {`${Math.ceil(content.length / 100)} minutes`}
-        </div>
-
-        <div className="bg-slate-400 h-1 w-full">
-
+        <div className="text-slate-500 text-sm font-thin pl-3 pt-5">
+            {`${Math.ceil(content.length / 100)} minutes(s) read`}
         </div>
     </div>
 }
 
 
+
 function Avatar ({name}: {name : string}) {
-    return  <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+    return  <div className="relative inline-flex items-center justify-center w-5 h-5 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
     <span className="font-medium text-gray-600 dark:text-gray-300">
         {name[0]}
     </span>
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -4,8 +4,11 @@ export const Blogs = () => {
     return <div>
         <BlogCard
         authorName="shiruvati"
-        title={"title of the Blog"}
-        content={"content of the bolg"}
+        title="Today, I took a deep dive into the world of Next.js and 
+        learned both client-side and server-side rendering. 🌐"
+        content="Next.js is truly a game-changer for building modern web applications, offering the best of both worlds—blazing fast client-side experiences with the power and flexibility of server-side rendering. 
+        I explored how to build dynamic and interactive UIs that load quickly and render efficiently, 
+        providing a seamless user experience"
         publishedDate={"2nd Feb 2024"}
         />
     </div>
