add0e0bd30e881947380bc8afc843ed410d0c2f2
Shiruvati Narasimha <mrnattyboy05@gmail.com>
2024-09-11 20:04:19 +0530

    Update Signin.tsx

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -6,7 +6,8 @@ export const Signin = () => {
   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div className="flex justify-between h-16">
       <div className="flex-shrink-0 flex items-center font-extrabold from-zinc-950">
-          Welcome To Bloging 
+          Welcome To You Dashboard 
+          We Happy See You Here 
       </div>
       <div className="flex items-center">
         <a href="#" className="text-black px-3 py-2 rounded-md text-sm font-extrabold">Home</a>
